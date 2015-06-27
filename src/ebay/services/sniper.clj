10a61dec422e76f23fd2b23f8a5b394aac4fce5a
Andrea Maria Piana <andrea.maria.piana@gmail.com>
2015-06-27 21:12:30 +0100

    Use macro to build functions

--- src/ebay/services/sniper.clj
+++ src/ebay/services/sniper.clj
@@ -1,35 +1,18 @@
 (ns ebay.services.sniper
   (:require [ebay.helpers.esniper]))
 
-(defn save
-  ([user]
-    (boolean (ebay.helpers.esniper/save user)))
-  ([user item]
-    (boolean (ebay.helpers.esniper/save user item))))
+(defmacro build-function [action]
+  `(defn ~action
+    ([user#]
+      (boolean ((intern 'ebay.helpers.esniper '~action) user#)))
+    ([user# item#]
+      (boolean ((intern 'ebay.helpers.esniper '~action) user# item#)))))
 
-(defn delete
-  ([user]
-    (ebay.helpers.esniper/delete user))
-  ([user item]
-    (ebay.helpers.esniper/delete user item)))
-
-(defn edit 
-  ([user]
-    (boolean (ebay.helpers.esniper/edit user)))
-  ([user item]
-    (boolean (ebay.helpers.esniper/edit user item))))
-
-(defn exists?
-  ([user]
-    (boolean (ebay.helpers.esniper/exists? user)))
-  ([user item]
-    (boolean (ebay.helpers.esniper/exists? user item))))
-
-(defn valid?
-  ([user]
-    (boolean (ebay.helpers.esniper/valid? user)))
-  ([user item]
-    (boolean (ebay.helpers.esniper/valid? user item))))
+(build-function save)
+(build-function delete)
+(build-function edit)
+(build-function exists?)
+(build-function valid?)
 
 (defn invalid?
   ([user]
